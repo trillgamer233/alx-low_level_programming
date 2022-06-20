b3f28883025b31c4f78ee3f15987af16cdff3413
trillgamer233 <machariaryan0158720@gmail.com>
2022-06-20 11:59:09 -0700

    third commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - Prints found c
+ * @s: pointer to char
+ * @c: char params to found
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
